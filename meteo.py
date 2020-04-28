dc253ce42acb030ef52e4a8d50fd9c298fab3769
Nate Arnold <nathan.arnold@my.rctc.edu>
2020-04-27 20:44:02 -0500

    modified schemas

--- meteo.py
+++ meteo.py
@@ -6,13 +6,29 @@ meteoDB = client.test
 meteoDB = client["weather_data"]
 meteoCollection = meteoDB["temperatureAndPrecip"]
 
-newEntry = {"location":"Kasson",
-            "date":"4/27/2020",
-            "time":"20:00",
-            "temperature":66,
-            "precipitation":2,
-            "wind": 8.1,
-            "wind_dir": "W",
-            "visibility" : 24.9}
+newEntry = {"locationID":"1",
+    "time":"2020-04-27T20:34Z",
+    "temperature":[
+        {"temp":62,"unit":"F"}
+        ],
+    "feels_like":[
+        {"temp":62,"unit":"F"}
+        ],
+    "heat_index":[
+        {"temp":60,"unit":"F"}
+        ],
+    "dew_point":[
+        {"temp":41,"unit":"F"}
+        ],
+    "humidity":40,
+    "precipitation":2,
+    "barometer":1010,
+    "wind":[
+        {"spd":6.2, "unit":"mph", "dir":"West"}
+        ],
+    "uv_index":0,
+    "visibility":[
+        {"dist":24.9, "unit":"miles"}
+        ]}
 x = meteoCollection.insert_one(newEntry)
-print(x.inserted_ids);
+print(x);
